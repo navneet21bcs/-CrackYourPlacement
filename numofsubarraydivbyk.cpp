fc257159b940aa6d137acb4460a39e6c97700693
navneet21bcs <109129747+navneet21bcs@users.noreply.github.com>
2024-07-16 00:37:49 +0530

    Update numofsubarraydivbyk.cpp

--- numofsubarraydivbyk.cpp
+++ numofsubarraydivbyk.cpp
@@ -18,3 +18,27 @@ public:
         return res;
     }
 };
+
+Better Approach in O(n^2)
+int subarraysDivByK(vector<int>& nums, int k) {
+        
+        int res = 0;
+        int sum;
+        //prefix sum
+        for(int i=1;i<nums.size();i++)
+        {
+            nums[i]=nums[i]+nums[i-1];
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i; j < nums.size(); j++) {
+              if(i==0)
+              sum=nums[j];
+              else
+              sum=nums[j]-nums[i-1];
+              if(sum%k==0)
+              res++;
+            }     
+        }
+        return res;
+    }
+};
