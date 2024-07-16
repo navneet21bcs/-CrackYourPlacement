486917de85a2d7d68504a8f855586e87ce659a51
navneet21bcs <109129747+navneet21bcs@users.noreply.github.com>
2024-07-16 15:32:38 +0530

    Update subarraysumequalsk.cpp

--- subarraysumequalsk.cpp
+++ subarraysumequalsk.cpp
@@ -22,3 +22,22 @@ public:
         
     }
 };
+//optimal soln
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int ct=0;
+        int n=nums.size();
+       unordered_map<int,int>mp;
+       mp[0]=1;
+       int presum=0;q
+       for(int i=0;i<n;i++)
+       {
+        presum=presum+nums[i];
+        int remove=presum-k;
+        ct=ct+mp[remove];
+        mp[presum]++;
+       }
+        return ct;
+    }
+};
