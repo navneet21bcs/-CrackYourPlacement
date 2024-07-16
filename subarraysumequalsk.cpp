cfa5a2b2495b5bd3012bab5a58388bfb436a4068
navneet21bcs <109129747+navneet21bcs@users.noreply.github.com>
2024-07-16 15:07:15 +0530

    Create subarraysumequalsk.cpp

---
+++ subarraysumequalsk.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int ct=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        { 
+             int sum=0;
+            for(int j=i;j<n;j++)
+            {
+               
+                
+                    sum=sum+nums[j];
+                    if(sum==k)
+                    {
+                    ct++;
+                    }
+                }
+            
+        }
+        return ct;
+        
+    }
+};
